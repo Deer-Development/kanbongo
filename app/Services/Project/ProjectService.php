18943e508340b6cbe7e2f6d69af69126c6ce3b72
alexbucur-splash <alex@rowshr.com>
2025-01-23 04:50:13 +0200

    Refactor ProjectService query structure for clarity.
    Reorganized query building logic to improve readability and maintainability. Moved conditional checks and relationships to a clearer structure, explicitly separating concerns for 'Super-Admin' and regular users. This simplifies debugging and enhances code consistency.

--- app/Services/Project/ProjectService.php
+++ app/Services/Project/ProjectService.php
@@ -25,20 +25,34 @@ class ProjectService extends BaseService
 
         $user = Auth::user();
 
-        $query = $this->getModelInstance()->with(['owner', 'containers' => function ($q) use ($user) {
-            if ($user->hasRole('Super-Admin')) {
-                $q->with(['members' => function ($q) {
-                    $q->with('user');
-                }]);
-            } else {
+        $query = $this->getModelInstance()->query();
+
+        if (!$user->hasRole('Super-Admin')) {
+            $query->whereHas('containers', function ($q) use ($user) {
                 $q->where('owner_id', $user->id)
                     ->orWhereHas('members', function ($q) use ($user) {
                         $q->where('user_id', $user->id);
-                    })->with(['members' => function ($q) {
+                    });
+            });
+        }
+
+        $query->with([
+            'owner',
+            'containers' => function ($q) use ($user) {
+                if ($user->hasRole('Super-Admin')) {
+                    $q->with(['members' => function ($q) {
                         $q->with('user');
                     }]);
-            }
-        }]);
+                } else {
+                    $q->where('owner_id', $user->id)
+                        ->orWhereHas('members', function ($q) use ($user) {
+                            $q->where('user_id', $user->id);
+                        })->with(['members' => function ($q) {
+                            $q->with('user');
+                        }]);
+                }
+            },
+        ]);
 
         $query = $query->applyFilters($request);
 
